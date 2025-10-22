3d37bd959d5d33fe1e5213dc01463f6337716592
unknown <jkmonisha97@gmail.com>
2025-10-22 08:15:29 +0530

    change axios

--- src/api/axios.js
+++ src/api/axios.js
@@ -3,10 +3,11 @@ import axios from "axios";
 const API = axios.create({
   baseURL:
     process.env.REACT_APP_API_URL ||
-    "http://localhost:8000/api/", // fallback for local dev
+    "https://ecommerce-backend-3-iqhc.onrender.com/api/",
   headers: {
     "Content-Type": "application/json",
   },
 });
 
 export default API;
+
