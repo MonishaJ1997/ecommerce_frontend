5d974a666e3822a85186bedf2808020b77c9789a
unknown <jkmonisha97@gmail.com>
2025-10-22 07:58:41 +0530

    add new file

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,5 @@
-// src/api/axios.js
 import axios from "axios";
 
-// Use environment variable from .env
 const API = axios.create({
   baseURL:
     process.env.REACT_APP_API_URL ||
@@ -10,17 +8,5 @@ const API = axios.create({
     "Content-Type": "application/json",
   },
 });
-
-// Optional: Automatically attach token if you use auth later
-API.interceptors.request.use(
-  (config) => {
-    const token = localStorage.getItem("token");
-    if (token) {
-      config.headers.Authorization = `Bearer ${token}`;
-    }
-    return config;
-  },
-  (error) => Promise.reject(error)
-);
 
 export default API;
